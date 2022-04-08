ee9dcc4acd40853921d24c0b5a2c38bfe4f346ae
“kidan16” <eyueltekle@hotmail.com>
2022-04-08 20:24:06 +0300

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48, i <= 55, i++)
+	{
+		for (j = i + 1, j <= 56, j++)
+		{
+			for (k = j + 1, k <= 57, k++)
+				putchar(i);
+				putchar(j);
+				if (i != 55)
+				{	putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 98, i < 123, i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+
+	return (0);
+}
