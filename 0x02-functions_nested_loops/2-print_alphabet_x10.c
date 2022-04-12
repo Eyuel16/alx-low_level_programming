b069259e0f6df05f3cd139e6b014a0eaeb4ed86f
“kidan16” <eyueltekle@hotmail.com>
2022-04-12 23:35:03 +0300

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@
  */
 void print_alphabet_x10(void)
 {
-	int check = 0, j;
+	int check, j;
 
-	for (; i < 10; check++)
+	for (check = 0; i < 10; check++)
 	{
 
 		for (j = 97; j <= 122; j++)
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_product = i * j;
+			product = i * j;
 			if ((product / 10) == 0)
 				_putchar(product % 10 + '0');
 			if ((product / 10) != 0)
@@ -26,7 +26,6 @@ void times_table(void)
 				_putchar(' ');
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
-	return (0);
 }
