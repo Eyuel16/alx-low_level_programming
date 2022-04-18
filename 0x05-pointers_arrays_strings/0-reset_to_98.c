a5342cc220516bb8e38702a919e08a3a99f677b7
“kidan16” <eyueltekle@hotmail.com>
2022-04-18 15:28:55 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value of variable.
+ * @n: is a pointer parameter.
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen -returns the length of a string.
+ * @s: is the value of the string variable.
+ *
+ * Return: void.
+ */
+int _strlen(char *s)
+
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	continue;
+	}
+
+	return (i);
+}
