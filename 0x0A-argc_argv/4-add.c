50028e660a0deab81ea13f516e383f8d467fd440
“kidan16” <eyueltekle@hotmail.com>
2022-04-29 20:28:46 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,21 +13,21 @@
 int main(int argc, char *argv[])
 {
 	unsigned int i, j, sum;
-	char *p;
 
 	sum = 0;
 	i = 1;
+	j = 1;
 
 	if (argc < 3)
 	{
 		printf("%d\n", 0);
 	}
 
-	for (p = argv[i]; i != argc; i++)
+	for (argv[i]; i != argc; i++)
 	{
-		for (j = 0; p[j] != '\0'; j++)
+		for (argv[i][j]; argv[i][j] != '\0'; j++)
 		{
-			if (!(isdigit(p[j])))
+			if (!(isdigit(argv[i][j])))
 			{
 				printf("Error\n");
 				return (1);
