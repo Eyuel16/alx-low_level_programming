532c7ff89c25a5a5d9674cc589a19b4e5b224df3
“kidan16” <eyueltekle@hotmail.com>
2022-04-29 21:06:24 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,39 +4,34 @@
 #include <ctype.h>
 
 /**
- * main - Entry point
- * @argc: number of arguments
- * @argv: an array containing arguments
+ * main - main function.
+ * @argc: an int parameter.
+ * @argv: array parameter.
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	unsigned int i, j, sum;
+	unsigned int i, sum, num;
 
 	sum = 0;
-	i = 1;
-	j = 0;
 
 	if (argc < 3)
 	{
 		printf("%d\n", 0);
-		return (0);
 	}
-
-	while (argc > 0)
+	while (argc-- && argc > 0)
 	{
-		while (argv[argc] != '\0')
+		for (i = 0; argv[argc][i] != '\0'; i++)
 		{
-			if (!(isdigit(argv[i][j])))
+			if (!(isdigit(argv[argc][i])))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			j++;
 		}
-		sum += atoi(argv[argc]);
-		argc--;
+		num = atoi(argv[argc]);
+		sum += num;
 	}
 	printf("%d\n", sum);
 	return (0);
