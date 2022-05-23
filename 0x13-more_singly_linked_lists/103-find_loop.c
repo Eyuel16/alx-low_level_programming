704b1f870f9e13c81fd2559a18c8947475ebabd4
“kidan16” <eyueltekle@hotmail.com>
2022-05-23 21:05:40 +0300

    0x13-more_singly_linked_lists.

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * mul - multipy a and b
+ *
+ * @a: integer 1
+ * @b: integer 2
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: list of type listint_t
+ *
+ * Return: address of node where loop starts, or NULL if no loop exists
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = (fast->next)->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
